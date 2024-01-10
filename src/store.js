7aff8b5ef2942b469683f2c2196e18e04d2b3b43
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-01-10 17:51:50 +0100

    store: Add new link for tv seires

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    filmList: [],
-    apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=e52b2ec8f0acc5ee4d9beeb743268796&query=movie&language=it-IT",
+    apiSearchMovieURL: "https://api.themoviedb.org/3/search/movie?api_key=e52b2ec8f0acc5ee4d9beeb743268796&query=",
+    movieList: [],
+    apiSearchTvURL: "https://api.themoviedb.org/3/search/tv?api_key=e52b2ec8f0acc5ee4d9beeb743268796&query=",
+    tvList: [],
     searchText: "",
-    loading: true,
+    apiImgBaseURL: "https://image.tmdb.org/t/p/w342"
 });
